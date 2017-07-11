990cdfa9a006cf197e4cc76d527e24f36e650175
peterwronski <1408463@rgu.ac.uk>
2017-07-11 20:11:22 +0100

    fixing link redirecting

---
+++ about.php
@@ -0,0 +1,10 @@
+<?php
+include('header.php');
+
+?>
+
+<h1>This is the ABOUT page</h1>
+
+</body>
+
+</html>
---
+++ components.php
@@ -0,0 +1,10 @@
+<?php
+include('header.php');
+
+?>
+
+<h1>This is the COMPONENTS page</h1>
+
+</body>
+
+</html>
