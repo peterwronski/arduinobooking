89bac454c6212bb4a9fc4f5bcdd2c7685fa13985
peterwronski <1408463@rgu.ac.uk>
2017-07-11 18:21:51 +0100

    Initial commit

---
+++ header.php
@@ -0,0 +1,62 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Arduino@RGU</title>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="style.css">
+
+
+
+</head>
+<body>
+
+
+
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php"></a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">About Arduino</a></li>
+                <li><a href="#">Components</a></li>
+                <li><a href="http://campusmoodle.rgu.ac.uk">CampusNoodle</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#" data-toggle="modal" data-target="#login-modal"><span class="glyphicon glyphicon-user"></span>Login</a>
+
+                    <div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
+                        <div class="modal-dialog">
+                            <div class="loginmodal-container">
+                                <h1>Login to Your Account</h1><br>
+                                <form>
+                                    <input type="text" name="user" placeholder="Username">
+                                    <input type="password" name="pass" placeholder="Password">
+                                    <input type="submit" name="login" class="login loginmodal-submit" value="Login">
+                                </form>
+
+                                <div class="login-help">
+                                    <a href="#">Register</a> - <a href="#">Forgot Password</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<!--check-->
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Peter
+ * Date: 11/07/2017
+ * Time: 16:29
+ */
+
+include('header.php'); ?>
+
+<h1> Hello World!</h1> <br/>
+
+<h2>Goodbye World!</h2>
+
+
+</body
+
+</html>
