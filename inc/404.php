c9f4e314717ea41e42679f0952ff80e348b2ddba
peterwronski <1408463@rgu.ac.uk>
2017-07-11 20:45:15 +0100

    fixing link redirecting

--- inc/404.php
+++ inc/404.php
@@ -5,8 +5,7 @@
     <meta charset="UTF-8">
     <title>Not Found</title>
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 
     <link rel="stylesheet" href="../style.css">
 
@@ -16,5 +15,5 @@
 <body>
 
 <div class="alert alert-warning">
-    <strong>Someone, somewhere has messed up</strong> <br/> This website doesn't exist. Click <a href="home.php">HERE</a> to go back to the website.
+    <strong>Someone, somewhere has messed up</strong> <br/> This page doesn't exist. Click <a href="home.php">HERE</a> to go back to the website.
 </div>
