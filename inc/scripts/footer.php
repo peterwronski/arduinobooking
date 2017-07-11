b7ac80b5cead6be7bc1f71170aa1ac84446affed
peterwronski <1408463@rgu.ac.uk>
2017-07-12 00:09:06 +0100

    Setting a footer

--- inc/scripts/footer.php
+++ inc/scripts/footer.php
@@ -9,8 +9,7 @@
 
             <li><a class="navbar-brand" href="http://www.rgu.ac.uk/"><img src="/inc/img/csdmlogo.png" height="50px" width="150px" class="img-responsive" alt="RGU School of Computing Science and Digital Media Logo and Link"/></a></li>
 
-            <li><a class="navbar-text" href="#">&copy;RGU 2017 <br/>
-                    Designed and Developed by Peter Wronski</a></li>
+            <li><a class="navbar-text" href="#">&copy;RGU 2017 // Designed and Developed by Peter Wronski</a></li>
         </ul>
     </div>
 </nav>
