2cbeb64714e4d5547271c94d765523c377263231
peterwronski <1408463@rgu.ac.uk>
2017-07-11 22:03:12 +0100

    fixing invisible logo

--- inc/scripts/header.php
+++ inc/scripts/header.php
@@ -25,7 +25,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="../../"><img src='../img/logomain.png' class="img-responsive" width="150px" height="70px" alt="Arduino Booking At RGU Logo"/></a>
+            <a class="navbar-brand" href="../../"><img src='/inc/img/logomain.png' class="img-responsive" width="150px" height="70px" alt="Arduino Booking At RGU Logo"/></a>
         </div>
         <div class="collapse navbar-collapse" id="myNavbar">
             <ul class="nav navbar-nav">
