fbe9f85c14d131b2f4267a4b5d1bc132002c4152
peterwronski <1408463@rgu.ac.uk>
2017-07-11 23:36:38 +0100

    fixing invisible logo

--- inc/scripts/footer.php
+++ inc/scripts/footer.php
@@ -3,15 +3,15 @@
 <footer>
     <div class="container">
         <div class="row">
-            <div class="col">
+            <div class="col-sm-4">
                <a href="https://www.arduino.cc/"><img src="/inc/img/arduinologo.png" height="70px" width="150px" alt="Arduino Community Logo and Link"/></a>
             </div>
 
-            <div class="col">
+            <div class="col-sm-4">
                 <a href="http://www.rgu.ac.uk/"><img src="/inc/img/csdmlogo.png" height="70px" width="150px" alt="RGU School of Computing Science and Digital Media Logo and Link"/></a>
             </div>
 
-            <div class="col">
+            <div class="col-sm-4">
                 &copy;RGU 2017 <br/>
                 Designed and Developed by Peter Wronski
             </div>
