e7573c11d7306a06a2e4970ec091e1ea26487c33
peterwronski <1408463@rgu.ac.uk>
2017-07-12 00:07:42 +0100

    Setting a footer

--- inc/scripts/footer.php
+++ inc/scripts/footer.php
@@ -5,7 +5,7 @@
 <nav class="navbar navbar-inverse navbar-fixed-bottom">
     <div class="container-fluid">
         <ul class="nav navbar-nav">
-            <li><a class="navbar-brand" href="https://www.arduino.cc/"><img src="/inc/img/arduinologo.png" height="50px" width="140px" class="img-responsive" alt="Arduino Community Logo and Link"/></a></li>
+            <li><a class="navbar-brand" href="https://www.arduino.cc/"><img src="/inc/img/arduinologo.png" height="30px" width="120px" class="img-responsive" alt="Arduino Community Logo and Link"/></a></li>
 
             <li><a class="navbar-brand" href="http://www.rgu.ac.uk/"><img src="/inc/img/csdmlogo.png" height="50px" width="150px" class="img-responsive" alt="RGU School of Computing Science and Digital Media Logo and Link"/></a></li>
 
