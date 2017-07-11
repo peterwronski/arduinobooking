9b3d7cc4abd156954ff0d77305d2799f0dde40d9
peterwronski <1408463@rgu.ac.uk>
2017-07-11 23:46:59 +0100

    Setting a footer

--- inc/scripts/footer.php
+++ inc/scripts/footer.php
@@ -3,15 +3,15 @@
 <footer>
     <div class="container">
         <div class="row">
-            <div class="col-sm-4">
+            <div class="col-sm-4 footer">
                <a href="https://www.arduino.cc/"><img src="/inc/img/arduinologo.png" height="50px" width="140px" alt="Arduino Community Logo and Link"/></a>
             </div>
 
-            <div class="col-sm-4">
+            <div class="col-sm-4 footer">
                 <a href="http://www.rgu.ac.uk/"><img src="/inc/img/csdmlogo.png" height="50px" width="150px" alt="RGU School of Computing Science and Digital Media Logo and Link"/></a>
             </div>
 
-            <div class="col-sm-4">
+            <div class="col-sm-4 footer">
                 &copy;RGU 2017 <br/>
                 Designed and Developed by Peter Wronski
             </div>
