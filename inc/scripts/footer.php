f0180e3f592e29ced04ecf9a4638918e1b0e5cf7
peterwronski <1408463@rgu.ac.uk>
2017-07-12 11:38:21 +0100

    Setting a footer

--- inc/scripts/footer.php
+++ inc/scripts/footer.php
@@ -1,18 +1,18 @@
 </body>
 
 
+<div class="navbar navbar-default navbar-fixed-bottom">
+    <div class="container">
 
-<footer class="fixed-bottom">
-        <hr/>
-        <ul class="nav navbar-nav">
-            <li><a class="navbar-brand" href="https://www.arduino.cc/"><img src="/inc/img/arduinologo.png" height="15px" width="85px" class="img-responsive" alt="Arduino Community Logo and Link"/></a></li>
+        <a href="https://www.arduino.cc/"><img src="/inc/img/arduinologo.png" height="15px" width="85px" class="img-responsive" alt="Arduino Community Logo and Link"/></a>
 
-            <li><a class="navbar-brand" href="http://www.rgu.ac.uk/"><img src="/inc/img/csdmlogo.png" height="15px" width="115px" class="img-responsive" alt="RGU School of Computing Science and Digital Media Logo and Link"/></a></li>
-        </ul>
-        <ul class="nav navbar-nav navbar-right">
-            <li><a class="navbar-text">&copy;RGU 2017 // Designed and Developed by Peter Wronski</a></li>
-        </ul>
-</footer>
+        <a href="http://www.rgu.ac.uk/"><img src="/inc/img/csdmlogo.png" height="15px" width="115px" class="img-responsive" alt="RGU School of Computing Science and Digital Media Logo and Link"/></a>
+
+        <p class="navbar-text pull-left">&copy;RGU 2017 // Designed and Developed by Peter Wronski</p>
+    </div>
+
+
+</div>
 
 </html>
 
