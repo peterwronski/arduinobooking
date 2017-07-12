d890d0a11a2203c80a0c716d0b1e8644431039e6
peterwronski <1408463@rgu.ac.uk>
2017-07-12 11:46:28 +0100

    Setting a footer

--- inc/scripts/footer.php
+++ inc/scripts/footer.php
@@ -4,11 +4,20 @@
 <div class="navbar navbar-default navbar-fixed-bottom">
     <div class="container">
 
-        <a href="https://www.arduino.cc/"><img src="/inc/img/arduinologo.png" height="15px" width="85px" class="img-responsive" alt="Arduino Community Logo and Link"/></a>
+        <div class="row">
+            <div class="col-sm-4">
+                <a href="https://www.arduino.cc/"><img src="/inc/img/arduinologo.png" class="img-responsive" alt="Arduino Community Logo and Link"/></a>
+            </div>
 
-        <a href="http://www.rgu.ac.uk/"><img src="/inc/img/csdmlogo.png" height="15px" width="115px" class="img-responsive" alt="RGU School of Computing Science and Digital Media Logo and Link"/></a>
+            <div class="col-sm-4">
+                <a href="http://www.rgu.ac.uk/"><img src="/inc/img/csdmlogo.png" class="img-responsive" alt="RGU School of Computing Science and Digital Media Logo and Link"/></a>
+            </div>
 
-        <p class="navbar-text pull-left">&copy;RGU 2017 // Designed and Developed by Peter Wronski</p>
+            <div class="col-sm-4">
+                <p class="navbar-text pull-left">&copy;RGU 2017 // Designed and Developed by Peter Wronski</p>
+            </div>
+        </div>
+
     </div>
 
 
