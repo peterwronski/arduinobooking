f1ecddfe4f29ee4d7d7c7152d1284c2b5e288e76
peterwronski <1408463@rgu.ac.uk>
2017-07-13 15:48:46 +0100

    Login system

--- inc/login.php
+++ inc/login.php
@@ -1,26 +1,12 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'GET') { ?>
 
-<div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
-    <div class="modal-dialog">
-        <div class="loginmodal-container">
-            <h1>Login to Your Account</h1><br>
-            <div class="alert alert-info">
-                <strong>Heads up!</strong>This system is currently not connected to the RGU network, therefore your existing RGU credentials won't work unless you create a new account on this system.
-            </div>
 
             <form action="login" method="POST">
                 <input type="text" name="email" placeholder="Email">
                 <input type="password" name="pass" placeholder="Password">
                 <input type="submit" name="login" class="login loginmodal-submit" value="Login">
             </form>
-
-            <div class="login-help">
-                <a href="#">Register</a> - <a href="#">Forgot Password</a>
-            </div>
-        </div>
-    </div>
-</div>
 
 <? }
 elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
