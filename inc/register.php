2dc185705cff9bebc76835ccb3e2166ce10fc10e
peterwronski <1408463@rgu.ac.uk>
2017-07-14 00:51:49 +0100

    Registration system

---
+++ inc/register.php
@@ -0,0 +1,46 @@
+<?php
+include ('dbconnect.php');
+if (($_POST['password']!=$_POST['password2'])) {// this checks to see if both password fields are a match
+echo'<script type="text/javascript">
+    alert("Your passwords aren\'t matching. Please make sure your passwords match before submitting the form.");
+
+    window.location = "./"
+</script>';
+};
+
+
+$studentid = $_POST['studentid'];
+$fname = $_POST['fname'];
+$sname = $_POST['sname'];
+$email = $_POST['email'];
+$pass = $_POST['pass'];
+$course = $_POST['course'];
+
+$studentid = $conn->real_escape_string($studentid);
+$fname = $conn->real_escape_string($fname);
+$sname = $conn->real_escape_string($sname);
+$email = $conn->real_escape_string($email);
+$pass = $conn->real_escape_string($pass);
+$course = $conn->real_escape_string($course);
+
+$hashAndSalt = password_hash($pass, PASSWORD_DEFAULT);
+
+$check_studentid = $conn->query("SELECT studentid FROM users WHERE studentid='$studentid'"); #CHECKING THAT THE STUDENTID ISN'T REGISTERED
+$count=$check_studentid->num_rows;
+
+if($count==0) {
+    $adduser = "INSERT INTO users VALUE('$studentid', '$fname', '$sname', '$email', '$pass', '$course')";
+    if ($conn->query($adduser) === TRUE) {
+        echo ' <script type="text/javascript">
+            alert("You\'ve made an account.");
+
+    window.location = "./"
+        </script>';
+    };
+} else {
+    echo'<script type="text/javascript">
+            alert("This Student ID is already registered.");
+    window.location = " ./"
+        </script>';
+}
+?>
