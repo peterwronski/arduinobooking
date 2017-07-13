7372b7738848990f8a6c79b8786b72258526bcaf
peterwronski <1408463@rgu.ac.uk>
2017-07-13 14:31:51 +0100

    Login system

--- index.php
+++ index.php
@@ -21,9 +21,9 @@ $rules = array(
     //
     //Admin Pages
     //
-    'login' => "/login",
-    'viewuser' => "/viewuser",
-    'logout' => "/logout",
+    'login' => "scripts/login",
+    'viewuser' => "scripts/viewuser",
+    'logout' => "scripts/logout",
     //
     // Home Page
     //
