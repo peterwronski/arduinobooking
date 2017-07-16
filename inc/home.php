35afd4bf9c20210be0eb1bb7ae3005a41d8bfcb2
peterwronski <1408463@rgu.ac.uk>
2017-07-16 01:00:15 +0100

    Registration system

--- inc/home.php
+++ inc/home.php
@@ -2,13 +2,11 @@
 session_start();
 include('scripts/header.php');
 
-$pageWasRefreshed = isset($_SERVER['HTTP_CACHE_CONTROL']) && $_SERVER['HTTP_CACHE_CONTROL'] === 'max-age=0';
-if($pageWasRefreshed){
-    unset($_SESSION['msg']);
-}
+
 
 if(isset($_SESSION['msg'])){
     echo $_SESSION['msg'];
+    unset($_SESSION['msg']);
 }
 
 ?>
