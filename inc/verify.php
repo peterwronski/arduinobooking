412d67bfc5093733d68d1a96bd5a481be1cf71ed
peterwronski <1408463@rgu.ac.uk>
2017-07-16 20:24:22 +0100

    Registration system

--- inc/verify.php
+++ inc/verify.php
@@ -24,14 +24,14 @@ $activation_hash = $conn->real_escape_string($activation_hash);
                                     <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
                                     <strong>Oh no!</strong>. Your account couldn\'t be activated.
                                 </div>';
-            header('Location: inc/home.php');
+            header('Location: ./');
         } else {
 
             $_SESSION['msg'] = '<div class="alert alert-success alert-dismissable" >
                                     <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
                                     <strong>Awesome!</strong>. Your account has been activated. You can now log in using the credentials you provided during registration.
                                 </div>';
-            header('Location: inc/home.php');
+            header('Location: ./');
         };
     } else {
         $_SESSION['msg'] = '<div class="alert alert-danger alert-dismissable" >
