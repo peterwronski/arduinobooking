c2ec8308aa30500ccef9770ccb3e498438d8cb71
peterwronski <1408463@rgu.ac.uk>
2017-07-16 20:44:37 +0100

    Registration system

--- inc/verify.php
+++ inc/verify.php
@@ -20,8 +20,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     $match = $search->num_rows;
 
     echo $activation_hash;
-    echo $search;
-    echo $match;
+    var_dump($search);
+    var_dump($match);
 
 /*
     if ($match > 0) {
