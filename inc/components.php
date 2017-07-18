078ca37074b73c35143f2e4d3a6dca2d1307b211
peterwronski <1408463@rgu.ac.uk>
2017-07-18 22:18:34 +0100

    Components list

--- inc/components.php
+++ inc/components.php
@@ -6,6 +6,16 @@ include('scripts/dbconnect.php');
 $query = $conn->query("SELECT * FROM components");
 
 $count = mysql_num_rows($query);
+?>
+
+<div class="container" id="componentlist">
+    <div class="row"><h1>Components</h1></div>
+    <div class="row"><button type="button" class="btn btn-info" data-toggle="collapse" data-target="#options">Options</button>
+        <div id="options" class="collapse">INSERT SORT OPTIONS HERE</div>
+
+    </div>
+
+</div>
 
 
 
