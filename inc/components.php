914ab65430af59539639164cc869635299fd4081
peterwronski <1408463@rgu.ac.uk>
2017-07-18 14:04:36 +0100

    Registration system

--- inc/components.php
+++ inc/components.php
@@ -1,7 +1,13 @@
 <?php
 session_start();
 include('scripts/header.php');
-if(!isset($_SESSION['userloggedin']) || empty($_SESSION['userloggedin'])){
+if(isset($_SESSION['userloggedin']) && !empty($_SESSION['userloggedin'])){ ?>
+
+    <h1>This is the COMPONENTS page</h1>
+
+<?php
+} else {
+
     $_SESSION['msg'] = '<div class="alert alert-danger alert-dismissable" >
                                     <a href="" class="close" data-dismiss="alert" aria-label="close">×</a>
                                     <strong>Hold up!</strong> You have to be logged in to access the content of this page. 
@@ -9,13 +15,7 @@ if(!isset($_SESSION['userloggedin']) || empty($_SESSION['userloggedin'])){
                                     <a href="#" data-toggle="modal" data-target="#reg-modal">HERE</a> to register.
                                 </div>';
     header('Location: ./');
-} else {
-
-    ?>
-
-    <h1>This is the COMPONENTS page</h1>
 
 
-    <?php
 };
 include('scripts/footer.php'); ?>
