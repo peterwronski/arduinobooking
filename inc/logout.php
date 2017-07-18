23d4ddfdfc432f3bdf1d76c8ceae7e1743747691
peterwronski <1408463@rgu.ac.uk>
2017-07-18 20:45:24 +0100

    Registration system

--- inc/logout.php
+++ inc/logout.php
@@ -2,7 +2,7 @@
 session_start();
 session_destroy();
 
-$_SESSION['msg'] = 'User logged in is: ' .$_SESSION['userloggedin'] .' //End of string';
-header('Location:./');
+echo "User logged in: " .$_SESSION['userloggedin'] .'//endofstring';
+//header('Location:./');
 exit();
 ?>
