38beee3117f0079b9134816eaca3cd433c320414
peterwronski <1408463@rgu.ac.uk>
2017-07-19 12:07:06 +0100

    Components list

--- inc/components.php
+++ inc/components.php
@@ -10,7 +10,7 @@ $count = mysql_num_rows($query);
 
 <div class="container" id="componentlist">
     <div class="row align-items-center"><div class="col-lg-8 col-lg-offset-2" id="componentdiv"><h1>Components</h1></div></div>
-    <div class="row"><button type="button" class="btn btn-info" data-toggle="collapse" data-target="#options">Options</button>
+    <div class="row"><button type="button" class="btn btn-basic btn-block" data-toggle="collapse" data-target="#options">Options</button>
         <div id="options" class="collapse">INSERT SORT OPTIONS HERE</div>
 
     </div>
