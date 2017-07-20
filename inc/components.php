2411c02ca986900eb20d200d912d5b25337daafd
peterwronski <1408463@rgu.ac.uk>
2017-07-21 00:54:19 +0100

    Sorting options for components

--- inc/components.php
+++ inc/components.php
@@ -19,40 +19,63 @@ if(isset($_SESSION['userloggedin']) && !empty($_SESSION['userloggedin'])) {
 
 
                         echo' <form action="components" method="post">
-                                Sort by: <select name="sort_method1" class="btn-new">
-                                            <option value="name">Name</option>
-                                            <option value = "in_stock">In stock</option>
-                                         </select>
+                                Sort by: <a class="btn btn-info btn-select">
+                                                <input type="hidden" class="btn-select-input" name="sort_method1"/>
+                                                    <span class="btn-select-value">Select a value</span>
+                                                    <span class="btn - select - arrow glyphicon glyphicon - chevron - down"></span>
+                                                <ul>
+                                                    <li>Name</li>
+                                                    <li>In stock</li>
+                                                </ul>
+                                         </a>
                                          
-                                         <select name="sort_method2" class="btn-new">
-                                            <option value="ascending">Ascending</option>
-                                            <option value="descending"> Descending</option>
-                                         </select>
+                                         <a class="btn btn-info btn-select">
+                                                <input type="hidden" class="btn-select-input" name="sort_method2"/>
+                                                    <span class="btn-select-value">Select a value</span>
+                                                    <span class="btn - select - arrow glyphicon glyphicon - chevron - down"></span>
+                                                <ul>
+                                                    <li>Ascending</li>
+                                                    <li>Descending</li>
+                                                </ul>
+                                         </a>
                                          
                                          <input type="submit" name="sort_submit">  </input>
                                          </form>
+                                         
+                                         
                         
                         ';
+
+                    //SORTING FORM - CONFIRMED WORKING
+                    /*<select name="sort_method1" class="btn-new">
+                                        <option value="name">Name</option>
+                                        <option value = "in_stock">In stock</option>
+                                     </select>
+
+                                     <select name="sort_method2" class="btn-new">
+                                        <option value="ascending">Ascending</option>
+                                        <option value="descending"> Descending</option>
+                                     </select>*/
                    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
                             $sort_method1 = $_POST['sort_method1'];
                             $sort_method2 = $_POST['sort_method2'];
 
                                 switch ($sort_method1) {
-                                    case "name":
+                                    case "Name":
                                         $_SESSION['sort_method1'] = " comp_name";
                                         break;
 
-                                    case "in_stock":
+                                    case "In stock":
                                         $_SESSION['sort_method1'] = " in_stock";
                                     break;
                                 };
 
                                 switch ($sort_method2) {
-                                    case "ascending":
+                                    case "Ascending":
                                         $_SESSION['sort_method2'] = " ASC";
                                         break;
-                                    case "descending":
+                                    case "Descending":
                                         $_SESSION['sort_method2'] = " DESC";
                                         break;
                                 };
