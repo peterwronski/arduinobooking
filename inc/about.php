a71ed9002d12c2721724e7ce821967e322149482
peterwronski <1408463@rgu.ac.uk>
2017-07-20 15:10:04 +0100

    Populating 'about' page

--- inc/about.php
+++ inc/about.php
@@ -26,7 +26,7 @@ include('scripts/header.php');
 
         </div>
         <div class="col-md-4">
-            <img src="/inc/img/ard_about.png" alt="Arduino Board" class="img-responsive " style="margin-top: 10%"/>
+            <img src="/inc/img/ard_about.png" alt="Arduino Board" class="img-responsive " style="margin-top: 50%"/>
         </div>
     </div>
     <div class="row">
