3f97be1e7eebc2c10ca4b7230f8546e20bd90697
peterwronski <1408463@rgu.ac.uk>
2017-07-21 17:13:55 +0100

    Implementing shopping cart

--- inc/cart.php
+++ inc/cart.php
@@ -1,36 +1,15 @@
 <?php
+include('scripts/dbconnect.php');
 //Source: http://phppot.com/php/simple-php-shopping-cart/
+if(isset($_GET['action']) && isset($_GET['comp_id']))
 switch ($_GET['action']){
     case "add":
-    if (!empty($_POST["quantity"])) {
-        $compByCode = $conn->query("SELECT * FROM components WHERE comp_ref='" . $_GET["comp_id"] . "'");
-        $itemArray = array($compByCode[0]["comp_id"] =>
-                        array('comp_name' => $compByCode[0]["comp_name"],
-                            'comp_id' => $compByCode[0]["comp_ref"],
-                            'quantity' => $_POST["quantity"]));
-        print_r("Item added to array");
-
-        if (!empty($_SESSION["cart_item"])) {
-            if (in_array($compByCode[0]["comp_id"], array_keys($_SESSION["cart_item"]))) {
-                foreach ($_SESSION["cart_item"] as $k => $v) {
-                    if ($compByCode[0]["comp_id"] == $k) {
-                        if (empty($_SESSION["cart_item"][$k]["quantity"])) {
-                            $_SESSION["cart_item"][$k]["quantity"] = 0;
-                        }
-                        $_SESSION["cart_item"][$k]["quantity"] += $_POST["quantity"];
-                    }
-                    print_r("Item added to session");
-                }
-            } else {
-                $_SESSION["cart_item"] = array_merge($_SESSION["cart_item"], $itemArray);
-                print_r("Item merged to session");
-            }
-        } else {
-            $_SESSION["cart_item"] = $itemArray;
-            print_r("Item added to session");
+        $query = $conn->query('SELECT * FROM components WHERE comp_ref = "' .$_GET['comp_id'] .'"');
+        echo $query;
+        break;
         }
-    }
 
-    break;
 
-}
+
+
+
