d8ba4a418e038e4b8cd68222f2ec180d16e67a39
peterwronski <1408463@rgu.ac.uk>
2017-07-21 17:03:05 +0100

    Implementing shopping cart

--- inc/addtocart.php
+++ inc/addtocart.php
@@ -1,36 +1,36 @@
 <?php
 //Source: http://phppot.com/php/simple-php-shopping-cart/
-$comp_ref = $params['comp_id'];
-if (isset($comp_ref)) {
+switch ($_GET['action']){
+    case "add":
     if (!empty($_POST["quantity"])) {
-        $compByCode = $conn->query("SELECT * FROM components WHERE comp_ref='" . $comp_ref . "'");
+        $compByCode = $conn->query("SELECT * FROM components WHERE comp_ref='" . $_GET["comp_id"] . "'");
         $itemArray = array($compByCode[0]["comp_id"] =>
                         array('comp_name' => $compByCode[0]["comp_name"],
-                            'comp_id' => $productByCode[0]["comp_ref"],
+                            'comp_id' => $compByCode[0]["comp_ref"],
                             'quantity' => $_POST["quantity"]));
+        print_r("Item added to array");
 
         if (!empty($_SESSION["cart_item"])) {
-            if (in_array($compByCode[0]["code"], array_keys($_SESSION["cart_item"]))) {
+            if (in_array($compByCode[0]["comp_id"], array_keys($_SESSION["cart_item"]))) {
                 foreach ($_SESSION["cart_item"] as $k => $v) {
-                    if ($compByCode[0]["code"] == $k) {
+                    if ($compByCode[0]["comp_id"] == $k) {
                         if (empty($_SESSION["cart_item"][$k]["quantity"])) {
                             $_SESSION["cart_item"][$k]["quantity"] = 0;
                         }
                         $_SESSION["cart_item"][$k]["quantity"] += $_POST["quantity"];
                     }
+                    print_r("Item added to session");
                 }
             } else {
                 $_SESSION["cart_item"] = array_merge($_SESSION["cart_item"], $itemArray);
+                print_r("Item merged to session");
             }
         } else {
             $_SESSION["cart_item"] = $itemArray;
+            print_r("Item added to session");
         }
     }
-} else {
-    $_SESSION['msg'] = '<div class="alert alert-danger alert-dismissable">
-                                    <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
-                                    <strong>Action not recognised</strong> Try something else
-                                </div>';
-    session_write_close();
-    header('Location: components');
+
+    break;
+
 }
