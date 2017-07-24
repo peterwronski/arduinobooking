ca7615f6f037ee465277eebb1b425275747dfd12
peterwronski <1408463@rgu.ac.uk>
2017-07-24 01:11:37 +0100

    Implementing shopping cart

--- inc/cart.php
+++ inc/cart.php
@@ -13,32 +13,33 @@ if(isset($action) && isset($comp_ref)) {
 	if(!empty($_POST["quantity"])) {
 
         $itemArray = array($row[0]["comp_ref"]=>array('comp_name'=>$row[0]["comp_name"], 'comp_ref'=>$row[0]["comp_ref"], 'quantity'=>$_POST["quantity"]));
-
+        echo 'IS THIS THING WORKING 1';
         if(!empty($_SESSION["cart"])) {
+            echo 'IS THIS THING WORKING 2';
             if(in_array($row[0]["comp_ref"],array_keys($_SESSION["cart"]))) {
+                echo 'IS THIS THING WORKING 3';
                 foreach($_SESSION["cart"] as $k => $v) {
                     if($row[0]["comp_ref"] == $k) {
+                        echo 'IS THIS THING WORKING 4';
                         if(empty($_SESSION["cart"][$k]["quantity"])) {
                             $_SESSION["cart"][$k]["quantity"] = 0;
+                            echo 'IS THIS THING WORKING 5';
                         }
                         $_SESSION["cart"][$k]["quantity"] += $_POST["quantity"];
+                        echo 'IS THIS THING WORKING 6';
                     }
                 }
             } else {
                 $_SESSION["cart"] = array_merge($_SESSION["cart"],$itemArray);
+                echo 'IS THIS THING WORKING 7';
             }
         } else {
             $_SESSION["cart"] = $itemArray;
+            echo 'IS THIS THING WORKING 8';
         }
     }
     echo $_SESSION["cart"];
-
-
-
-
-
-
-
+    echo 'IS THIS THING WORKING 9';
                    /* include('scripts/header_2.php');
                     echo '<div class="alert alert-warning alert-dismissable">
                                     <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
@@ -46,16 +47,11 @@ if(isset($action) && isset($comp_ref)) {
                                 </div>';
 
                     include('scripts/footer.php'); */
-
-
             break;
 
         case "view":
-            echo ' IS THIS THING WORKING';
-            foreach($_SESSION['cart'] as $key=>$value){
-                echo 'Key: ' .$key .' // Value: ' .$value;
-            }
-            break;
+            echo ' IS THIS THING WORKING view';
+
     }
 
 } else {
