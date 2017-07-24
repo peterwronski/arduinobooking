2bdef06f51a4123ed603a45da2ea86df419e5943
peterwronski <1408463@rgu.ac.uk>
2017-07-24 01:41:31 +0100

    Implementing shopping cart

--- inc/cart.php
+++ inc/cart.php
@@ -16,27 +16,28 @@ if(isset($action)) {
         $itemArray = array($row[0]["comp_ref"]=>array('comp_name'=>$row[0]["comp_name"], 'comp_ref'=>$row[0]["comp_ref"], 'quantity'=>$_POST["quantity"]));
         echo 'IS THIS THING WORKING 1';
         if(!empty($_SESSION["cart"])) {
-            echo 'IS THIS THING WORKING 2';
+            //echo 'IS THIS THING WORKING 2';
             if(in_array($row[0]["comp_ref"],array_keys($_SESSION["cart"]))) {
-                echo 'IS THIS THING WORKING 3';
+                //echo 'IS THIS THING WORKING 3';
                 foreach($_SESSION["cart"] as $k => $v) {
                     if($row[0]["comp_ref"] == $k) {
-                        echo 'IS THIS THING WORKING 4';
+                        //echo 'IS THIS THING WORKING 4';
                         if(empty($_SESSION["cart"][$k]["quantity"])) {
                             $_SESSION["cart"][$k]["quantity"] = 0;
-                            echo 'IS THIS THING WORKING 5';
+                            var_dump($_SESSION["cart"]);
                         }
                         $_SESSION["cart"][$k]["quantity"] += $_POST["quantity"];
-                        echo 'IS THIS THING WORKING 6';
+                        //echo 'IS THIS THING WORKING 6';
+                        var_dump($_SESSION["cart"]);
                     }
                 }
             } else {
                 $_SESSION["cart"] = array_merge($_SESSION["cart"],$itemArray);
-                echo 'IS THIS THING WORKING 7';
+                var_dump($_SESSION["cart"]);
             }
         } else {
             $_SESSION["cart"] = $itemArray;
-            echo 'IS THIS THING WORKING 8';
+            var_dump($_SESSION["cart"]);
         }
 
             break;
