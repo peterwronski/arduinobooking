41d0e86b4a54bd11f3701580024c588a79d164e9
peterwronski <1408463@rgu.ac.uk>
2017-07-25 14:02:27 +0100

    Implementing shopping cart

--- inc/cart.php
+++ inc/cart.php
@@ -15,7 +15,7 @@ function itemAdded(){
                                 </div>';
     global $conn, $comp_ref, $quantity;
     $get_inStock = $conn->query('SELECT in_stock FROM components WHERE comp_ref ="' .$comp_ref .'"');
-    $row = $get_inStock->fetch_row();
+    $row = $get_inStock->fetch_array();
 
 
     $conn->query('UPDATE components SET in_stock = "'.($row['in_stock'] - $quantity) .'" WHERE comp_ref="' .$comp_ref .'"');
@@ -78,7 +78,7 @@ if(isset($action)) {
             if($comp_ref === "all"){
                 foreach ($_SESSION["cart"] as $key => $value) {
                     $get_inStock = $conn->query('SELECT in_stock FROM components WHERE comp_ref ="' .$key .'"');
-                    $row = $get_inStock->fetch_row();
+                    $row = $get_inStock->fetch_array();
                     $conn->query('UPDATE components SET in_stock ="' .($row['in_stock'] + $value['quantity']) .'" WHERE comp_ref ="' .$key .'"');
                 }
                 unset($_SESSION["cart"]);
@@ -94,7 +94,7 @@ if(isset($action)) {
                 if (!empty($_SESSION["cart"])) {
                     foreach ($_SESSION["cart"] as $key => $value) {
                         $get_inStock = $conn->query('SELECT in_stock FROM components WHERE comp_ref ="' .$key .'"');
-                        $row = $get_inStock->fetch_row();
+                        $row = $get_inStock->fetch_array();
                         $conn->query('UPDATE components SET in_stock ="' .$row['in_stock'] + $value['quantity'] .'" WHERE comp_ref ="' .$key .'"');
                         if ($comp_ref == $key) unset($_SESSION["cart"][$key]);
                         if (empty($_SESSION["cart"])) unset($_SESSION["cart"]);
