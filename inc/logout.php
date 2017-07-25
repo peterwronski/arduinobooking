335ebb65ff532692a176146d9632b059c5f5fd6d
peterwronski <1408463@rgu.ac.uk>
2017-07-25 21:20:33 +0100

    Implementing shopping cart

--- inc/logout.php
+++ inc/logout.php
@@ -1,6 +1,14 @@
 <?php
 session_start();
 session_destroy();
+if(isset($_SESSION['cart'])){
+    foreach ($_SESSION["cart"] as $key => $value) {
+        $get_inStock = $conn->query('SELECT in_stock FROM components WHERE comp_ref ="' .$key .'"');
+        $row = $get_inStock->fetch_array();
+        $conn->query('UPDATE components SET in_stock ="' .($row['in_stock'] + $value['quantity']) .'" WHERE comp_ref ="' .$key .'"');
+    }
+    unset($_SESSION["cart"]);
+}
 header('Location:./');
 exit();
 
