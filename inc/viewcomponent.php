b9126a74309b5998c3285edd0d591b741049b539
peterwronski <1408463@rgu.ac.uk>
2017-07-27 21:52:12 +0100

    Implementing shopping cart

--- inc/viewcomponent.php
+++ inc/viewcomponent.php
@@ -9,7 +9,7 @@
 include('scripts/header_2.php');
 $comp_ref = $params['comp_ref'];
 date_default_timezone_set('Europe/London');
-$date_now = date('D-M-Y');
+$date_now = (new DateTime())->format('d-m-y');
 
 if (isset($_SESSION['userloggedin']) && !empty($_SESSION['userloggedin'])){
 include('scripts/dbconnect.php');
