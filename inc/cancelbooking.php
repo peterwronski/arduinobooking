e9c3b9b8c56d22fead69ecaa6efa65341c16fec7
peterwronski <1408463@rgu.ac.uk>
2017-08-10 21:40:05 +0100

    Implementing shopping cart

--- inc/cancelbooking.php
+++ inc/cancelbooking.php
@@ -3,15 +3,24 @@ session_start();
 include ('scripts/dbconnect.php');
 $booking_id = $params['booking_id'];
 
-$delete = 'DELETE * FROM booking WHERE booking_id ="'.$booking_id .'" AND studentid = "'.$_SESSION['studentid'] .'";';
-$conn->query($delete);
+$delete = 'DELETE FROM booking WHERE booking_id ="'.$booking_id .'" AND studentid = "'.$_SESSION['studentid'] .'";';
+$result = $conn->query($delete);
 
-$_SESSION['msg'] = '<div class="alert alert-success alert-dismissable">
+if ($result) {
+    $_SESSION['msg'] = '<div class="alert alert-success alert-dismissable">
     <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
     <strong>Yeee boi!</strong> Booking cancelled!
 </div>';
 
 
-header("Location:../../bookings");
+    header("Location:../../bookings");
+} else {
+    $_SESSION['msg'] = '<div class="alert alert-danger alert-dismissable">
+    <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
+    <strong>Something went wrong!</strong> Booking was NOT cancelled.
+</div>';
 
+
+    header("Location:../../bookings");
+}
 ?>
