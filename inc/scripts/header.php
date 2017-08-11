1a45eae85f620d2931601bf0121e56f763c83548
peterwronski <1408463@rgu.ac.uk>
2017-08-11 16:56:01 +0100

    Implementing shopping cart

--- inc/scripts/header.php
+++ inc/scripts/header.php
@@ -54,7 +54,7 @@
                     <span class="caret"></span></a>
         <ul class="dropdown-menu">
           <li><a href="user/view/all"><span class="glyphicon glyphicon-user"></span> Users</a></li>
-          <li><a href="adminbooking/view/all"><span class="glyphicon glyphicon-barcode"></span>Bookings</span></a></li>
+          <li><a href="adminbooking/view/all"><span class="glyphicon glyphicon-barcode"></span> Bookings</span></a></li>
           
         </ul>
       </li>';
