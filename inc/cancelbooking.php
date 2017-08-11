c59db48dfa003d0e7b16cd42404a9a11664f8580
peterwronski <1408463@rgu.ac.uk>
2017-08-11 14:32:33 +0100

    Implementing shopping cart

--- inc/cancelbooking.php
+++ inc/cancelbooking.php
@@ -3,15 +3,17 @@ session_start();
 include ('scripts/dbconnect.php');
 $booking_id = $params['booking_id'];
 
-$delete = 'DELETE FROM booking WHERE booking_id ="'.$booking_id .'" AND studentid = "'.$_SESSION['studentid'] .'";';
-$result = $conn->query($delete);
-
 $get_inStock = $conn->query('SELECT booking.comp_ref, components.in_stock, booking.quantity FROM components, booking WHERE booking.booking_id ="'.$booking_id .'" AND components.comp_ref = booking.comp_ref');
 $row = $get_inStock->fetch_array();
 
 $quantity = $row['in_stock'] + $row['quantity'] ;
 
 $in_stockUpdate = $conn->query('UPDATE components SET in_stock = "' . $quantity . '" WHERE comp_ref="' . $row['comp_ref'] . '"');
+
+
+$delete = 'DELETE FROM booking WHERE booking_id ="'.$booking_id .'" AND studentid = "'.$_SESSION['studentid'] .'";';
+$result = $conn->query($delete);
+
 
 if ($result && $in_stockUpdate) {
     $_SESSION['msg'] = '<div class="alert alert-success alert-dismissable">
