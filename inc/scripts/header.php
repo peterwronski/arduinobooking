ed5e7e2cc839879d77475ae914a36ede2c6dd7b6
peterwronski <1408463@rgu.ac.uk>
2017-08-11 16:30:56 +0100

    Implementing shopping cart

--- inc/scripts/header.php
+++ inc/scripts/header.php
@@ -48,7 +48,7 @@
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <?php
-                if (isset($_SESSION['admin']) && $_SESSION['admin'] == TRUE){
+                if (isset($_SESSION['admin']) && $_SESSION['admin'] = TRUE){
                   echo'  <li class="dropdown">
         <a class="dropdown-toggle" data-toggle="dropdown" href="#">ADMIN MENU
                     <span class="caret"></span></a>
