13793a320ab39585ad7dd17c116fcb46bfc1f43d
peterwronski <1408463@rgu.ac.uk>
2017-08-15 23:12:40 +0100

    Fixing

--- inc/scripts/header_2.php
+++ inc/scripts/header_2.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Arduino@RGU</title>
-    <meta name="viewport" content="width=device-width, maximum-scale=1">
+    <meta name="viewport" content="width=device-width,initial-scale=1, maximum-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
