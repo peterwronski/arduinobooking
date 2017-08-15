ff3f52e06d217a645effd31a2f55ec872e4c4922
peterwronski <1408463@rgu.ac.uk>
2017-08-15 18:56:56 +0100

    Implementing shopping cart

--- inc/bookings.php
+++ inc/bookings.php
@@ -39,9 +39,9 @@ echo '
                 <table class="componenttable" width="100%">
                 <tr>
                 <td colspan="8">
-                <p><span class="glyphicon glyphicon-time"></span> - Waiting // 
-                                <span class="glyphicon glyphicon-remove"></span> - Denied // 
-                                <span class="glyphicon glyphicon-ok"></span> - Approved </p>
+                <span class="glyphicon glyphicon-time"></span><p> - Waiting // </p>
+                                <span class="glyphicon glyphicon-remove"></span><p> - Denied // </p>
+                                <span class="glyphicon glyphicon-ok"></span><p> - Approved </p>
                 </td>
                 </tr>
                 <tr>
