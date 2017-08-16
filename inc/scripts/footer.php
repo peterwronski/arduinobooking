89ebfc3eeda3554e396319d890d9416c9655f287
peterwronski <1408463@rgu.ac.uk>
2017-08-16 16:55:22 +0100

    Fixing

--- inc/scripts/footer.php
+++ inc/scripts/footer.php
@@ -5,11 +5,11 @@
 
 
             <div class="col-sm-4">
-                <a href="https://www.arduino.cc/"><img src="/inc/img/arduinologo.png"  height="50px" class="img-responsive" alt="Arduino Community Logo and Link"/></a>
+                <a href="https://www.arduino.cc/"><img src="/inc/img/arduinologo.png"  height="50px" alt="Arduino Community Logo and Link"/></a>
             </div>
 
             <div class="col-sm-4">
-                <a href="http://www.rgu.ac.uk/"><img src="/inc/img/csdmlogo.png" height="30px" class="img-responsive" alt="RGU School of Computing Science and Digital Media Logo and Link"/></a>
+                <a href="http://www.rgu.ac.uk/"><img src="/inc/img/csdmlogo.png" height="30px" alt="RGU School of Computing Science and Digital Media Logo and Link"/></a>
             </div>
 
             <div class="col-sm-4">
