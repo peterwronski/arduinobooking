54db85f2fb767cc8eb76179530e98839826215d2
peterwronski <1408463@rgu.ac.uk>
2017-08-16 16:18:15 +0100

    Fixing

--- inc/scripts/footer.php
+++ inc/scripts/footer.php
@@ -1,27 +1,4 @@
 </body>
 
-
-<div class="navbar navbar-default navbar-fixed-bottom">
-    <div class="container">
-
-        <div class="row">
-            <div class="col-sm-4">
-                <a href="https://www.arduino.cc/"><img src="/inc/img/arduinologo.png"  height="50px" class="img-responsive" alt="Arduino Community Logo and Link"/></a>
-            </div>
-
-            <div class="col-sm-4">
-                <a href="http://www.rgu.ac.uk/"><img src="/inc/img/csdmlogo.png" height="30px" class="img-responsive" alt="RGU School of Computing Science and Digital Media Logo and Link"/></a>
-            </div>
-
-            <div class="col-sm-4">
-                <p class="navbar-text">&copy;RGU 2017//Developed by Peter Wronski</p>
-            </div>
-        </div>
-
-    </div>
-
-
-</div>
-
 </html>
 
