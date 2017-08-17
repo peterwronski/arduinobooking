d31de9d64bc4c52bbfb74597586f5cba9ed81a37
peterwronski <1408463@rgu.ac.uk>
2017-08-17 18:15:08 +0100

    Fixing

--- inc/booking.php
+++ inc/booking.php
@@ -17,32 +17,41 @@ if(isset($_SESSION['userloggedin']) && !empty($_SESSION['userloggedin'])) {
     if (isset($action)) {
         switch ($action) {
             case "add":
-                //echo 'Date To: ' . $dateTo . ' <br/>Date From: ' . $dateFrom;
-                foreach ($_SESSION["cart"] as $key => $value) {
-                    $query = 'INSERT INTO booking (studentid, comp_ref, quantity, date_from, date_to, approved) VALUES ("' . $_SESSION['studentid'] . '", "' . $key . '" , "' . $value['quantity'] . '", "' . $dateFrom . '", "' . $dateTo . '", "0")';
-                    $result = $conn->query($query);
+                if(strtotime($dateTo) > strtotime($dateFrom)) {
+                    foreach ($_SESSION["cart"] as $key => $value) {
+                        $query = 'INSERT INTO booking (studentid, comp_ref, quantity, date_from, date_to, approved) VALUES ("' . $_SESSION['studentid'] . '", "' . $key . '" , "' . $value['quantity'] . '", "' . $dateFrom . '", "' . $dateTo . '", "0")';
+                        $result = $conn->query($query);
 
-                    $get_inStock = $conn->query('SELECT in_stock FROM components WHERE comp_ref ="' . $key . '"');
-                    $row = $get_inStock->fetch_array();
+                        $get_inStock = $conn->query('SELECT in_stock FROM components WHERE comp_ref ="' . $key . '"');
+                        $row = $get_inStock->fetch_array();
 
-                    $quantity = $row['in_stock'] - $value['quantity'];
+                        $quantity = $row['in_stock'] - $value['quantity'];
 
-                    $in_stockUpdate = $conn->query('UPDATE components SET in_stock = "' . $quantity . '" WHERE comp_ref="' . $key . '"');
-
-
-                    unset ($_SESSION['cart'][$key]);
+                        $in_stockUpdate = $conn->query('UPDATE components SET in_stock = "' . $quantity . '" WHERE comp_ref="' . $key . '"');
 
 
-                };
+                        unset ($_SESSION['cart'][$key]);
 
-                $_SESSION['msg'] = '<div class="alert alert-success alert-dismissable">
+
+                    };
+
+                    $_SESSION['msg'] = '<div class="alert alert-success alert-dismissable">
                                     <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
                                     <strong>Yeee boi!</strong> Booking created!
                                 </div>';
 
 
-                header("Location:../../viewcart");
+                    header("Location:../../viewcart");
 
+                } else {
+                    $_SESSION['msg'] = '<div class="alert alert-danger alert-dismissable">
+                                    <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
+                                    <strong>Check your dates!</strong> \'Returning on\' date can\'t be before \'Booking from \' date.
+                                </div>';
+
+
+                    header("Location:../../viewcart");
+                }
 
                 break;
 
