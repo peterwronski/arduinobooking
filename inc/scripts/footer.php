5e0522a55f6f784b70b3163470e8c91e60323fdf
peterwronski <1408463@rgu.ac.uk>
2017-08-17 16:43:16 +0100

    Fixing

--- inc/scripts/footer.php
+++ inc/scripts/footer.php
@@ -1,28 +1,4 @@
 </body>
 
-<nav class="navbar navbar-default">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myFooter">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-        </div>
-        <div class="collapse navbar-collapse" id="myFooter">
-            <div class="col-sm-12">
-                <a href="https://www.arduino.cc/"><img src="/inc/img/arduinologo.png"  height="50px" alt="Arduino Community Logo and Link"/></a>
-            </div>
-
-            <div class="col-sm-4">
-                <a href="http://www.rgu.ac.uk/"><img src="/inc/img/csdmlogo.png" height="30px" alt="RGU School of Computing Science and Digital Media Logo and Link"/></a>
-            </div>
-
-            <div class="col-sm-4">
-                <p class="navbar-text pull-right">&copy;RGU 2017//Developed by Peter Wronski</p>
-            </div>
-    </div>
-    </div>
-</nav>
 </html>
 
