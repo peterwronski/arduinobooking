78ebf1b8496b026a5c7a5bd111628b99622cb666
peterwronski <1408463@rgu.ac.uk>
2017-08-17 17:45:12 +0100

    Fixing

--- inc/scripts/header_2.php
+++ inc/scripts/header_2.php
@@ -72,9 +72,7 @@
                                         <input type="submit" name="login" class="login loginmodal-submit" value="Login">
                                     </form>
 
-                                    <div class="login-help">
-                                        <a href="#">Forgot Password</a>
-                                    </div>
+                                    
                                 </div>
                             </div>
                         </div>
