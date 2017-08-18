33dafd3b197fb6676b4674cb61a9898ff51a9c97
peterwronski <1408463@rgu.ac.uk>
2017-08-18 12:02:54 +0100

    Fixing

--- inc/home.php
+++ inc/home.php
@@ -7,5 +7,34 @@ if(isset($_SESSION['msg'])){
 };
 ?>
 
-<h1>This is the HOME page</h1>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-6 col-sm-offset-0 homebox1">
+
+
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-6 col-sm-offset-6 homebox2">
+
+
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-6 col-sm-offset-0 homebox1">
+
+
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-6 col-sm-offset-6 homebox2">
+
+
+        </div>
+    </div>
+</div>
+
+
+
+
 <?php include('scripts/footer.php'); ?>
