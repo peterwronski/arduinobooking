e01f85409952b0d65548b4eac68f167130a8a5b9
peterwronski <1408463@rgu.ac.uk>
2017-08-18 12:12:41 +0100

    Fixing

--- inc/home.php
+++ inc/home.php
@@ -19,25 +19,26 @@ if(isset($_SESSION['msg'])){
     </div>
     <div class="row">
         <div class="col-sm-6 col-sm-offset-0 homebox1">
+            <h1>Step 1</h1> <h2>Create an account</h2>
 
 
         </div>
     </div>
     <div class="row">
         <div class="col-sm-6 col-sm-offset-6 homebox2">
-
+            <h1>Step 2</h1> <h2>Find components</h2>
 
         </div>
     </div>
     <div class="row">
         <div class="col-sm-6 col-sm-offset-0 homebox1">
-
+            <h1>Step 3</h1> <h2>Pick dates</h2>
 
         </div>
     </div>
     <div class="row">
         <div class="col-sm-6 col-sm-offset-6 homebox2">
-
+            <h1>Step 4</h1> <h2>Sit back and wait for confirmation</h2>
 
         </div>
     </div>
