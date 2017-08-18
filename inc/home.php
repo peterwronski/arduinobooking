ef2ad76295238eac38f570e6f05398ae207f94fb
peterwronski <1408463@rgu.ac.uk>
2017-08-18 12:08:43 +0100

    Fixing

--- inc/home.php
+++ inc/home.php
@@ -10,10 +10,10 @@ if(isset($_SESSION['msg'])){
 
 <div class="container">
     <div class="row">
-        <div class="col-sm-6 col-sm-offset-0">
+        <div class="col-sm-6">
             <h1>Welcome to ArduinoBooking!</h1>
         </div>
-        <div class="col-sm-6 col-sm-offset-6">
+        <div class="col-sm-6">
             <h2>Here's a quick guide on how to use the website</h2>
         </div>
     </div>
